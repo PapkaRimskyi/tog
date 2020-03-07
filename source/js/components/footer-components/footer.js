700530fab1f1ce1fb26d128e3acff499ed5af996
Гуров Георгий <magnezia98@mail.ru>
2020-03-07 22:34:44 +0300

    Rewtired some html, added effects

--- source/js/components/footer-components/footer.js
+++ source/js/components/footer-components/footer.js
@@ -1,19 +1,21 @@
 import AbstractClass from '../../support-classes/abstract-class.js';
 
-const copyrightMarkup = () => `<section class="copyright">
+const copyrightMarkup = (year) => `<section class="copyright">
   <h2 class="visually-hidden">Остальная информация</h2>
-  <p class="copyright__text">Придумано компанией ЛГД в 2019 году. Все права защищены &#169;.</p>
+  <p class="copyright__text">Придумано компанией <span class="copyright__participant">Л</span><span class="copyright__participant copyright__participant--green-color">Г</span><span class="copyright__participant copyright__participant--gold-color">Д</span>.</p>
+  <p class="copyright__text">Все права защищены 2019-${year}&#169;</p>
 </section>
 `;
 
 export default class Copyright extends AbstractClass {
   constructor() {
     super();
+    this.year = new Date().getFullYear();
   }
 
   //Template
 
   getTemplate() {
-    return copyrightMarkup();
+    return copyrightMarkup(this.year);
   }
 }
--- source/js/components/header-components/header.js
+++ source/js/components/header-components/header.js
@@ -36,16 +36,48 @@ const headerMarkup = () => {
         <li class="header-nav__item">
           <a class="header-nav__link" href="#">Гороскоп</a>
         </li>
+        <li class="header-nav__item">
+          <a class="header-nav__link participants" id="participants-popup" href="#">Вписать участников</a>
+        </li>
       </ul>
     </nav>
-    <section class="participants">
-      <h2 class="visually-hidden">Добавить участников</h2>
-      <a class="participants__link participants" id="participants-popup" href="#">Вписать участников</a>
-    </section>
   </div>
 </header>
 `;
 };
+
+// const headerMarkup = () => {
+//   const timeData = createDateTime();
+//   return `<header class="tog-header">
+//   <h1 class="tog-header__title">Tournament of Games</h1>
+//   <div class="tog-header__container">
+//     <section class="time-and-data">
+//       <h2 class="visually-hidden">Дата и время</h2>
+//       <p class="time-and-data__info">
+//         <time class="time-and-data__info time-and-data__info--date" datetime="${timeData.dateTimeAttrValue}">${timeData.date}</time>|<time class="time-and-data__info time-and-data__info--time" dateTime="${timeData.time}">${timeData.time}</time>
+//       </p>
+//     </section>
+//     <nav class="header-nav header-nav--margin-left">
+//       <ul class="header-nav__list">
+//         <li class="header-nav__item">
+//           <a class="header-nav__link rules" id="rules-popup" href="#">Правила</a>
+//         </li>
+//         <li class="header-nav__item">
+//           <a class="header-nav__link audio-player" id="audio-popup" href="#">Плеер</a>
+//         </li>
+//         <li class="header-nav__item">
+//           <a class="header-nav__link" href="#">Гороскоп</a>
+//         </li>
+//       </ul>
+//     </nav>
+//     <section class="participants">
+//       <h2 class="visually-hidden">Добавить участников</h2>
+//       <a class="participants__link participants" id="participants-popup" href="#">Вписать участников</a>
+//     </section>
+//   </div>
+// </header>
+// `;
+// };
 
 
 export default class Header extends AbstractClass {
